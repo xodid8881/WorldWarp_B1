79ae6025b81ee375a8586985cb9cb802f9397883
Hwa_Baeg_ <aoadid8881@naver.com>
2023-01-10 14:30:28 +0900

    Update WorldWarp.php

--- WorldWarp_B1/src/WorldWarp/WorldWarp.php
+++ WorldWarp_B1/src/WorldWarp/WorldWarp.php
@@ -4,9 +4,11 @@ namespace WorldWarp;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\level\Level;
+
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+
+use pocketmine\world\World;
 
 class WorldWarp extends PluginBase implements Listener {
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
