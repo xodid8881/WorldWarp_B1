10d6c153ce1c27538fc3e6a39824827f4cd67a7d
Hwa_Baeg_ <aoadid8881@naver.com>
2023-01-28 19:59:03 +0900

    Update WorldWarp.php

--- WorldWarp_B1/src/WorldWarp/WorldWarp.php
+++ WorldWarp_B1/src/WorldWarp/WorldWarp.php
@@ -16,25 +16,31 @@ class WorldWarp extends PluginBase implements Listener {
 
 	public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
 	{
-		$tag = "§b§l[ §f월드§b ] §f";
+		$tag = "§b§l[ §fWorldWarp§b ] §f";
 		$command = $command->getName ();
 		$name = $sender->getName ();
-		if ($command == "월드이동") {
+		if ($command == "worldwarp") {
 			if (! isset ( $args [0] )) {
-				$sender->sendMessage ( $tag . "/월드이동 [ 월드이름 ]" );
+				$sender->sendMessage ( $tag . "/worldwarp [ WorldFolderName ]" );
 				return true;
 			}
 			switch ($args [0]) {
 				case $args [0] :
+				if (! file_exists ( $this->getServer ()->getDataPath () . "worlds/" . $args[0] )) {
+					$sender->sendMessage ( $tag . "There is no world with that name" );
+					return true;
+				} else {
+					Server::getInstance()->getWorldManager()->loadWorld ($args[0]);
+				}
 				foreach (Server::getInstance()->getWorldManager()->getWorlds() as $level) {
 					$world = $level->getFolderName();
-					if ($args [0] == $world){
-						$sender->sendMessage ( $tag . " 월드 " . $args [0] . " 로 이동했습니다." );
+					if ($args[0] == $world){
+						$sender->sendMessage ( $tag . "move successfully" );
 						$sender->teleport($level->getSafeSpawn());
 						return true;
 					}
 				}
-				$sender->sendMessage ( $tag . "해당 월드를 찾을 수 없습니다.." );
+				$sender->sendMessage ( $tag . "There is no world with that name" );
 				return true;
 			}
 		}
